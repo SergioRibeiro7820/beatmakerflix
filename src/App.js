24fbf76bba603f622f1056c3e44c09de4d2dcffa
SergioRibeiro7820 <63015680+SergioRibeiro7820@users.noreply.github.com>
2020-08-04 12:11:08 -0300

    App.js Atualizado
    O texto embaixo do texto de apresentação foi modificado conforme exercicio da imersão react alura.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <BannerMain 
       videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
       url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é Front-end"}
+      videoDescription={"#BEATMAKERS"}
     />
 
     <Carousel 
